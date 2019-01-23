75e5023eb0e344e4b44c2eb56b6f6b06ee5f7b40
Сахаров Денис <denis@sator.kz>
2019-01-23 09:42:13 +0500

    Промежуточный коммит для переноса проекта на второй сервер разработки. Немного навел порядок в грид.

--- frontend/web/js/app.js
+++ frontend/web/js/app.js
@@ -5,9 +5,14 @@
 Ext.application({
     name: 'Fiddle',
     launch: function () {
+        Ext.define('Task', {
+            extend: 'Ext.data.Model',
+            fields: ['id', 'title', 'date', 'author', 'status', 'description']
+        });
+
         var localFilesStore = Ext.create('Ext.data.Store', {
-            storeId: 'localFilesStore',
-            fields:['id', 'title', 'date', 'author', 'status', 'description'],
+            storeId: 'taskStore',
+            model: 'Task',
             proxy: {
                 type: 'ajax',
                 url: '/api/v1/task'
@@ -17,16 +22,41 @@ Ext.application({
 
         Ext.create('Ext.grid.Panel', {
             title: 'Задачи',
-            store: Ext.data.StoreManager.lookup('localFilesStore'),
+            store: Ext.data.StoreManager.lookup('taskStore'),
+            height: 400,
+            width: 650,
+            left: '50%',
+            renderTo: 'grid',
             columns: [
-                {header: 'Номер задачи', dataIndex: 'id'},
-                {header: 'Заголовок', dataIndex: 'title'},
-                {header: 'Дата выполнения', dataIndex: 'date'},
+                {
+                    header: 'Номер задачи',
+                    dataIndex: 'id',
+                    width: '130px',
+                    sortable: false,
+                    hideable: false,
+                    flex: 1
+                },
+                {
+                    header: 'Заголовок',
+                    dataIndex: 'title',
+                    width: '100px',
+                    sortable: false,
+                    hideable: false,
+                    flex: 1
+                },
+                {
+                    header: 'Дата выполнения',
+                    dataIndex: 'date',
+                    width: '210px',
+                    sortable: false,
+                    hideable: false,
+                    flex: 1
+                },
                 {xtype:'actioncolumn',
                     width:40,
                     items:[{
                         getClass: function() {
-                            return 'glyphicon glyphicon-remove';
+                            return 'glyphicon glyphicon-eye-open';
                         },
                         handler:function (grid, rowIndex, colIndex) {
                             console.log(grid.getStore().getAt(rowIndex).data['file']);
@@ -34,9 +64,12 @@ Ext.application({
                     }]
                 }
             ],
-            height: 400,
-            width: 1200,
-            renderTo: "grid"
+            dockedItems: [{
+                xtype: 'pagingtoolbar',
+                store: Ext.data.StoreManager.lookup('taskStore'),
+                dock: 'bottom',
+                displayInfo: true
+            }]
         });
     }
 });
